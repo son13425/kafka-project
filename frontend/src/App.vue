e6eaf055dcc3f5e8d16ca2f9f66682b746afeda0
Elelna Shirokova <stolar3@yandex.ru>
2026-02-20 00:30:33 +0300

    Настройка отправки тестовых данных

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
 import { ref, computed, onUnmounted } from "vue";
 
+import { arrWorlds, arrBlockedWorlds, arrAction } from "./constants"
+
 const URL_API = `${import.meta.env.VITE_API}`;
+
+// Практическая работа №1
 
 const dataTransmission = ref(false);
 const intervalMany = ref(null);
@@ -17,11 +21,6 @@ const nameButtonTransmission = computed(() =>
     ? "Остановить передачу сообщений"
     : "Включить передачу сообщений",
 );
-
-onUnmounted(() => {
-  if (intervalMany.value) clearInterval(intervalMany.value);
-  if (intervalOne.value) clearInterval(intervalOne.value);
-});
 
 function setStatusTransmission() {
   dataTransmission.value = !dataTransmission.value;
@@ -49,8 +48,7 @@ function setStatusTransmission() {
 function sendOneMessage(keyMessage) {
   const textMsg =
     keyMessage === "one" ? "Одиночное сообщение" : "Пакетное сообщение";
-  const currentDate = new Date();
-  const isoDate = currentDate.toISOString();
+  const isoDate =  new Date().toISOString();
   try {
     fetch(`${URL_API}/message`, {
       method: "POST",
@@ -74,11 +72,182 @@ function sendOneMessage(keyMessage) {
     console.error("Ошибка в sendOneMessage(): ", error);
   }
 }
+
+// Практическая работа №2
+
+const dataTestTransmission = ref(false);
+const intervalTestMassage = ref(null);
+const testMessageCount = ref(0);
+const blockedMessageCount = ref(0);
+
+const nameTestStatus = computed(() =>
+  dataTestTransmission.value
+    ? "Тестовые сообщения отправляются"
+    : "Тестовые сообщения не отправляютя",
+);
+const nameButtonTestTransmission = computed(() =>
+  dataTestTransmission.value
+    ? "Остановить тестовую переписку"
+    : "Включить тестовую переписку",
+);
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function sendTestMessage() {
+  // Увеличиваем счетчик сообщений
+  testMessageCount.value++;
+
+  // Генерация user_id и recipient_id (разные числа от 1 до 21)
+  let user_id, recipient_id;
+  do {
+    user_id = getRandomInt(1, 21).toString();
+    recipient_id = getRandomInt(1, 21).toString();
+  } while (user_id === recipient_id);
+
+  // Текущее время в формате строки
+  const timestamp = new Date().toISOString();
+
+  // Выбор трех случайных слов из arrWorlds
+  const words = [];
+  for (let i = 0; i < 3; i++) {
+    const randomIndex = Math.floor(Math.random() * arrWorlds.length);
+    words.push(arrWorlds[randomIndex]);
+  }
+
+  // Формирование сообщения
+  let message = words.join(" ");
+
+  // Каждое третье сообщение добавляем слово из arrBlockedWorlds
+  if (testMessageCount.value % 3 === 0) {
+    const blockedWordIndex = Math.floor(Math.random() * arrBlockedWorlds.length);
+    const blockedWord = arrBlockedWorlds[blockedWordIndex];
+    message += " " + blockedWord;
+  }
+
+  // Отправка POST-запроса
+  try {
+    fetch(`${URL_API}/user-message`, {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        user_id,
+        recipient_id,
+        timestamp,
+        message,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json().then((res) => {
+            console.log("Тестовое сообщение отправлено!");
+            console.log(res.msg);
+          });
+        } else {
+          console.error(
+            "Ошибка отправки тестового сообщения: ",
+            response.status
+          );
+        }
+      })
+      .catch((err) => {
+        console.error("Ошибка сети при отправке тестового сообщения: ", err);
+      });
+  } catch (error) {
+    console.error("Ошибка в sendTestMessage(): ", error);
+  }
+}
+
+function sendStatusBlocked() {
+  // Увеличиваем счетчик сообщений
+  blockedMessageCount.value++;
+
+  // Генерация user_id и blocked_user_id (разные числа от 1 до 21)
+  let user_id, blocked_user_id;
+   if (blockedMessageCount.value % 3 === 0) {
+    const blockedWordIndex = Math.floor(Math.random() * arrBlockedWorlds.length);
+    const blockedWord = arrBlockedWorlds[blockedWordIndex];
+    user_id = 'forbidden_words'
+    blocked_user_id = blockedWord
+  } else {
+    do {
+      user_id = getRandomInt(1, 21).toString();
+      blocked_user_id = getRandomInt(1, 21).toString();
+    } while (user_id === blocked_user_id);
+  }
+
+  // Текущее время в формате строки
+  const timestamp = new Date().toISOString();
+
+  // Назначаем действие
+  const actionIndex = Math.floor(Math.random() * arrAction.length);
+  const action = arrAction[actionIndex];
+
+  // Отправка POST-запроса
+  try {
+    fetch(`${URL_API}/blocked-message`, {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        user_id,
+        blocked_user_id,
+        timestamp,
+        action
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json().then((res) => {
+            console.log("Cообщение о блокировке/разблокировке отправлено!");
+            console.log(res.msg);
+          });
+        } else {
+          console.error(
+            "Ошибка отправки сообщения о блокировке/разблокировке: ",
+            response.status
+          );
+        }
+      })
+      .catch((err) => {
+        console.error("Ошибка сети при отправке сообщения о блокировке/разблокировке: ", err);
+      });
+  } catch (error) {
+    console.error("Ошибка в sendStatusBlocked(): ", error);
+  }
+}
+
+function setStatusTestTransmission() {
+  dataTestTransmission.value = !dataTestTransmission.value;
+  if (dataTestTransmission.value) {
+    // Запускаем отправку каждую секунду
+    intervalTestMassage.value = setInterval(() => {
+      sendTestMessage();
+      sendStatusBlocked();
+    }, 1000);
+    // Сразу отправляем первое сообщение
+    sendTestMessage();
+    sendStatusBlocked();
+  } else {
+    // Останавливаем отправку
+    if (intervalTestMassage.value) {
+      clearInterval(intervalTestMassage.value);
+      intervalTestMassage.value = null;
+    }
+  }
+}
+
+onUnmounted(() => {
+  if (intervalMany.value) clearInterval(intervalMany.value);
+  if (intervalOne.value) clearInterval(intervalOne.value);
+  if (intervalTestMassage.value) clearInterval(intervalTestMassage.value);
+});
 </script>
 
 <template>
-  <div class="body-window">
-    <div class="pr-1">
+  <div>
+    <div>
+      <p>Практическая работа №1</p>
       <p>
         Состояние отправки сообщений:
         <span
@@ -94,46 +263,33 @@ function sendOneMessage(keyMessage) {
         {{ nameButtonTransmission }}
       </button>
     </div>
-    <div class="pr-2">
+    <div>
+      <p>Практическая работа №2</p>
       <p>
-        Состояние отправки сообщений:
+        Состояние отправки тестовых сообщений:
         <span
           :class="{
-            'status-transmission-active': dataTransmission === true,
-            'status-transmission-unactive': dataTransmission === false,
+            'status-test-transmission-active': dataTestTransmission === true,
+            'status-test-transmission-unactive': dataTestTransmission === false,
           }"
         >
-          {{ nameStatus }}
+          {{ nameTestStatus }}
         </span>
       </p>
-      <button @click="setStatusTransmission">
-        {{ nameButtonTransmission }}
+      <button @click="setStatusTestTransmission">
+        {{ nameButtonTestTransmission }}
       </button>
     </div>
   </div>
 </template>
 
 <style scoped>
-.body-window {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  width: 100%;
-  height: 100%;
-}
-
-.pr-1 {
-  background-color: red;
-}
-
-.pr-2 {
-  background-color: green;
-}
-
+.status-test-transmission-active,
 .status-transmission-active {
   color: green;
 }
 
+.status-test-transmission-unactive,
 .status-transmission-unactive {
   color: red;
 }
